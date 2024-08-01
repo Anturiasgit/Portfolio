316e5e9da44f9c63baf89bd2809100ddf0a49897
Anturia Hassani <anturia.hm@gmail.com>
2024-08-01 13:36:28 +0200

    Dernière étape suivie et intégrée : intégration des détails du Header : icones de LinkedIn, GitHub, bouton contactez-moi, nom et titre*

--- src/components/HeaderComp.jsx
+++ src/components/HeaderComp.jsx
@@ -1,16 +1,35 @@
 import React from 'react'
 import avatar from "../assets/images/avatar.png"
+import { GithubOutlined, LinkedinOutlined, MessageOutlined } from '@ant-design/icons'
+import { Button } from "antd";
 
 const HeaderComp = () => {
     return (
         <header>
             <div className='left'>
-                <img src={avatar} alt="" />
+                <img src={avatar} alt="user" />
+                <div className="user-infos">
+                    <h1>Anturia Hassani Mzé</h1>
+                    <h3>Développeuse web - mobile</h3>
+                    <div className="contacts">
+                        <div className="contact-item">
+                            <LinkedinOutlined />
+                        </div>
+                        <div className="contact-item">
+                            <GithubOutlined />
+                        </div>
+                    </div>
+                </div>
             </div>
             <div className='right'>
+                <Button className="contact-btn" icon={<MessageOutlined />}>
+                    Contactez-moi
+                </Button>
             </div>
         </header>
     )
 }
 
 export default HeaderComp
+
+
