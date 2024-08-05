82084aab42c2a0dfe04ed0cd9bbfeac34a039e29
Anturia Hassani <anturia.hm@gmail.com>
2024-08-05 12:12:39 +0200

    Dernière étape suivie et intégrée : ajout d'articles (mes récents projets) et (à propos de moi). (Mes récents projets) contient un titre, une description et un lien

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { LikeOutlined } from "@ant-design/icons";
 import "./App.css";
 import HeaderComp from "./components/HeaderComp";
 
@@ -8,12 +9,26 @@ function App() {
         <HeaderComp />
         <div className="body-container">
           <div className="left">
-          <section className="about">
+          <section>
               <article>
                 <h2>A propos de moi</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem animi nulla tenetur, dolor error commodi veniam rerum accusamus reprehenderit cupiditate dicta quos cumque magnam quasi non ducimus. Optio, unde vero.
                   Iure delectus et tenetur assumenda eligendi, nobis doloremque voluptas dignissimos officiis qui consequuntur labore ab ipsum ea molestias obcaecati praesentium est pariatur, laborum ad. In sit amet et culpa aperiam. </p>
               </article>
+            </section>
+
+            <section className="projects">
+            <h2>Mes récents projets</h2>
+            <article>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVTU89MIWSq3LP_chJmHwSTWGLHolHRbkPXw&s" alt="slug" />
+            <div className="project-info">
+              <h3>Site web de gestion de médias sociaux</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nobis deleniti eligendi eum ex ipsam, facere odio qui autem quam dolores placeat omnis possimus nostrum minus! Quisquam obcaecati placeat corporis!
+              Tempora exercitationem voluptate temporibus, expedita quibusdam facere cum eum, fugit odit repudiandae harum minus beatae asperiores. Iure mollitia iste suscipit, necessitatibus nesciunt nam quidem voluptatem molestiae ipsum similique, laboriosam delectus!
+              Saepe ipsa laudantium debitis sed architecto ducimus. Cum cupiditate quam laborum nobis, dolores incidunt rem aliquid in sapiente earum similique quia consequatur tempore nihil, fugit dolore harum error eligendi modi.</p>
+              <Button icon={<LikeOutlined/>}> </Button>             
+            </div>
+            </article>
             </section>
           </div>
           <div className="right">
