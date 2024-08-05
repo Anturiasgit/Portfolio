de87ea98d0e5cf33bc9a2ceee56a350d917a1836
Anturia Hassani <anturia.hm@gmail.com>
2024-08-05 11:00:50 +0200

    Dernière étape suivie et intégrée : Derniers arrangements du design du Header avec effet hover sur icônes, couleurs, espacements et positions des éléments

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,25 @@ import HeaderComp from "./components/HeaderComp";
 function App() {
   return (
     <>
-    <div className='main'>
-    <HeaderComp />
-    </div>
+      <div className='main'>
+        <HeaderComp />
+        <div className="body-container">
+          <div className="left">
+          <section className="about">
+              <article>
+                <h2>A propos de moi</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem animi nulla tenetur, dolor error commodi veniam rerum accusamus reprehenderit cupiditate dicta quos cumque magnam quasi non ducimus. Optio, unde vero.
+                  Iure delectus et tenetur assumenda eligendi, nobis doloremque voluptas dignissimos officiis qui consequuntur labore ab ipsum ea molestias obcaecati praesentium est pariatur, laborum ad. In sit amet et culpa aperiam. </p>
+              </article>
+            </section>
+          </div>
+          <div className="right">
+         
+          </div>
+        </div>
+      </div>
     </>
   );
 }
 
 export default App;
- 
