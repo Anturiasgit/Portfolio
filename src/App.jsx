d3544fbf463495d40906c0f986fc0ba98f4325c9
Anturia Hassani <anturia.hm@gmail.com>
2024-08-06 15:00:03 +0200

    Débogage d'un problème survenu sur la dernière étape où rien ne s'affichait suite à l'ajout d'un composant d'une bibliothèque (antd). Solution trouvée en gardant le composant

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { LikeOutlined } from "@ant-design/icons";
 import "./App.css";
 import HeaderComp from "./components/HeaderComp";
+import { Button } from "antd";
 
 function App() {
   return (
@@ -26,8 +27,8 @@ function App() {
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nobis deleniti eligendi eum ex ipsam, facere odio qui autem quam dolores placeat omnis possimus nostrum minus! Quisquam obcaecati placeat corporis!
               Tempora exercitationem voluptate temporibus, expedita quibusdam facere cum eum, fugit odit repudiandae harum minus beatae asperiores. Iure mollitia iste suscipit, necessitatibus nesciunt nam quidem voluptatem molestiae ipsum similique, laboriosam delectus!
               Saepe ipsa laudantium debitis sed architecto ducimus. Cum cupiditate quam laborum nobis, dolores incidunt rem aliquid in sapiente earum similique quia consequatur tempore nihil, fugit dolore harum error eligendi modi.</p>
-              <Button icon={<LikeOutlined/>}> </Button>             
-            </div>
+             <Button icon={<LikeOutlined />}>Soutenir mon projet</Button>            
+            </div> 
             </article>
             </section>
           </div>
