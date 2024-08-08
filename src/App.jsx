9eef50ec07d3bbb6f269c340ec703b59325f9f8f
Anturia Hassani <anturia.hm@gmail.com>
2024-08-08 20:49:46 +0200

    Dernière étape suivie et intégrée: Arrangement du design du 2ème article 'mes récents projets'

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
         <HeaderComp />
         <div className="body-container">
           <div className="left">
-          <section>
+            <section>
               <article>
                 <h2>A propos de moi</h2>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem animi nulla tenetur, dolor error commodi veniam rerum accusamus reprehenderit cupiditate dicta quos cumque magnam quasi non ducimus. Optio, unde vero.
@@ -19,21 +19,26 @@ function App() {
             </section>
 
             <section className="projects">
-            <h2>Mes récents projets</h2>
-            <article>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVTU89MIWSq3LP_chJmHwSTWGLHolHRbkPXw&s" alt="slug" />
-            <div className="project-info">
-              <h3>Site web de gestion de médias sociaux</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nobis deleniti eligendi eum ex ipsam, facere odio qui autem quam dolores placeat omnis possimus nostrum minus! Quisquam obcaecati placeat corporis!
-              Tempora exercitationem voluptate temporibus, expedita quibusdam facere cum eum, fugit odit repudiandae harum minus beatae asperiores. Iure mollitia iste suscipit, necessitatibus nesciunt nam quidem voluptatem molestiae ipsum similique, laboriosam delectus!
-              Saepe ipsa laudantium debitis sed architecto ducimus. Cum cupiditate quam laborum nobis, dolores incidunt rem aliquid in sapiente earum similique quia consequatur tempore nihil, fugit dolore harum error eligendi modi.</p>
-             <Button icon={<LikeOutlined />}>Soutenir mon projet</Button>            
-            </div> 
-            </article>
+              <h2>Mes récents projets</h2>
+              <article>
+                <img className="featured-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVTU89MIWSq3LP_chJmHwSTWGLHolHRbkPXw&s" alt="slug" />
+                <div className="project-info">
+                  <h3>Site web de gestion de médias sociaux</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nobis deleniti eligendi eum ex ipsam, facere odio qui autem quam dolores placeat omnis possimus nostrum minus! Quisquam obcaecati placeat corporis!
+                    Tempora exercitationem voluptate temporibus, expedita quibusdam facere cum eum, fugit odit repudiandae harum minus beatae asperiores. Iure mollitia iste suscipit, necessitatibus nesciunt nam quidem voluptatem molestiae ipsum similique, laboriosam delectus!
+                    Saepe ipsa laudantium debitis sed architecto ducimus. Cum cupiditate quam laborum nobis, dolores incidunt rem aliquid in sapiente earum similique quia consequatur tempore nihil, fugit dolore harum error eligendi modi.</p>
+                  <div className="btn-link">
+                    <Button className="btn" icon={<LikeOutlined />}>
+                      Soutenir mon projet
+                    </Button>
+                  </div>
+                </div>
+              </article>
             </section>
           </div>
+
           <div className="right">
-         
+
           </div>
         </div>
       </div>
