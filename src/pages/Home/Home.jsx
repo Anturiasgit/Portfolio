505c9ab26cb2168666245ed8492440986731e079
Anturia Hassani <anturia.hm@gmail.com>
2025-08-27 15:04:56 +0200

    Intégration compétences

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import Card from '../../components/Card/Card'
 import projects from '../../projects.json'
 import './Home.css'
-import { SiJavascript, SiReact, SiHtml5, SiCss, SiCplusplus, SiPhp, SiSymfony, SiNodedotjs, SiExpress, SiSass } from '@icons-pack/react-simple-icons'
+import { SiCanva, SiWordpress, SiFigma, SiGoogleWorkspace, SiMicrosoft365, SiFeedly, SiTeams, SiDiscord, SiSlack, SiNotion, SiJira, SiTrello, SiChromeDevTools, Sinpm, SiDocker, SiLinux, SiGit, SiPostman, SiVisualstudiocode, SiGithub, SiJavascript, SiReact, SiHtml5, SiCss, SiCplusplus, SiPhp, SiSymfony, SiNodedotjs, SiExpress, SiSass } from '@icons-pack/react-simple-icons'
 import { FiLink } from "react-icons/fi"
 
 
@@ -27,6 +27,7 @@ function Home() {
             <section className="competences">
                 <h1>Mes compétences</h1>
 
+                <h3>Langages</h3>
                 <div className="full-stack">
                     <p className="front-end">Front-End</p>
                     <p className="back-end">Back-End</p>
@@ -34,50 +35,102 @@ function Home() {
 
                 <div className="stack">
                     <div className="stack-item">
-                        <SiHtml5 size="24" color="#E34F26" />
-                        <span>HTML</span>
+                        <SiHtml5 size="35" color="#E34F26" />
+                        <p>HTML</p>
                     </div>
                     <div className="stack-item">
-                        <SiCss size="24" color="#1572B6" />
-                        <span>CSS</span>
+                        <SiCss size="35" color="#1572B6" />
+                        <p>CSS</p>
                     </div>
                     <div className="stack-item">
-                        <SiSass size="24" color="#CC6699" />
-                        <span>Scss</span>
+                        <SiSass size="35" color="#CC6699" />
+                        <p>Scss</p>
                     </div>
                     <div className="stack-item">
-                        <SiJavascript size="24" color="#F7DF1E" />
-                        <span>Javascript</span>
+                        <SiJavascript size="35" color="#F7DF1E" />
+                        <p>Javascript</p>
                     </div>
                     <div className="stack-item">
-                        <SiReact size="24" color="#61DAFB" />
-                        <span>ReactJS</span>
+                        <SiReact size="35" color="#61DAFB" />
+                        <p>ReactJS</p>
                     </div>
                     <div className="stack-item">
-                        <SiExpress size="24" color="#000000" />
-                        <span>ExpressJS</span>
+                        <SiExpress size="35" color="#000000" />
+                        <p>ExpressJS</p>
                     </div>
                     <div className="stack-item">
-                        <SiCplusplus size="24" color="#00599C" />
-                        <span>C++</span>
+                        <SiCplusplus size="35" color="#00599C" />
+                        <p>C++</p>
                     </div>
                     <div className="stack-item">
-                        <SiPhp size="29" color="#777BB4" />
-                        <span>PHP</span>
+                        <SiPhp size="33" color="#777BB4" />
+                        <p>PHP</p>
                     </div>
                     <div className="stack-item">
-                        <SiSymfony size="24" color="#000000" />
-                        <span>Symfony</span>
+                        <SiSymfony size="35" color="#000000" />
+                        <p>Symfony</p>
                     </div>
                     <div className="stack-item">
-                        <SiNodedotjs size="24" color="#339933" />
-                        <span>NodeJS</span>
+                        <SiNodedotjs size="35" color="#339933" />
+                        <p>NodeJS</p>
                     </div>
                 </div>
 
-                <div className="management">
-                    <p>Gestion de projet</p>
-                    <p>Listing outils management</p>
+                <div className="tools">
+                    <h3>Environement de travail</h3>
+
+                    <div className="dev-and-management-title">
+                        <p className="dev">Développement</p>
+                        <p className="management">Collaboration & Gestion de projet</p>
+                    </div>
+
+                    <div className="dev-and-management-tools">
+                        <div className="dev-tools">
+                            <SiVisualstudiocode />
+                            <p>VSCode</p>
+                            <SiGithub />
+                            <p>GitHub</p>
+                            <SiGit />
+                            <p>Git</p>
+                            <SiPostman />
+                            <p>Postman</p>
+                            <SiLinux />
+                            <p>Linux</p>
+                            <SiDocker />
+                            <p>Docker</p>
+                            <Sinpm />
+                            <p>npm</p>
+                            <SiChromeDevTools />
+                            <p>Chrome DevTools</p>
+                        </div>
+                        <div className="management-tools">
+                            <p>Trello</p>
+                            <p>Jira</p>
+                            <p>Notion</p>
+                            <p>Slack</p>
+                            <p>Discord</p>
+                            <p>Teams</p>
+                        </div>
+                    </div>
+
+                    <div className="prod-veille-and-uiux-cms-title">
+                        <p className="prod-veille">Productivité & Veille</p>
+                        <p className="uiux-cms">UI/UX & CMS</p>
+                    </div>
+
+                    <div className="prod-veille-and-uiux-cms-tools">
+                        <div className="prod-veille-tools">
+                            <p>Feedly</p>
+                            <p>Microsoft 365</p>
+                            <p>Google Workspace</p>
+                        </div>
+                        <div className="uiux-cms-tools">
+                            <p>Figma</p>
+                            <p>Canva</p>
+                            <p>Wordpress</p>
+                        </div>
+                    </div>
+
                 </div>
             </section>
         </>
